3ffb746bba93d02937af7fc8691a63ec1cf180fa
seb1212 <seb.nicollin@gmail.com>
2018-08-12 22:07:40 +0200

    Initialise projet

---
+++ App.js
@@ -0,0 +1,222 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, Alert, Image, BackHandler,
+  FlatList, ActivityIndicator, SafeAreaView, ScrollView, WebView, Vibration } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import { List, ListItem, SearchBar } from 'react-native-elements';
+
+class MyWeb extends Component {
+  render() {
+          Vibration.vibrate(1000);
+    return (
+      <WebView
+        source={{uri: 'https://github.com/facebook/react-native'}}
+        style={{marginTop: 20}}
+      />
+    );
+  }
+}
+
+class WelcomeView extends Component {
+
+  static navigationOptions = {
+    title: 'Bienvenue',
+  };
+
+  render() {
+    const sautDeLigne = '\n';
+    const { navigate } = this.props.navigation;
+
+    return (
+      <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Cliquez sur le bouton "Commencer"</Text><Text>{sautDeLigne}</Text>
+        <Text style={styles.text}>pour démarrer le questionnaire</Text><Text>{sautDeLigne}</Text>
+        <Button style={styles.button} onPress={() => navigate('FlatLDemo')} title="Commencer"/>
+      </View>
+    </SafeAreaView>
+    );
+  }
+}
+
+class FlatListDemo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false,
+    };
+  }
+
+  static navigationOptions = {
+  title: 'Liste',
+  };
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState({ loading: true });
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  renderSeparator = () => {
+    return(
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+      }} />
+    );
+  };
+
+  renderHeadeer = () => {
+    return <SearchBar placeholder="Type here..." lightTheme round />;
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) {return null;}
+    else {
+      return (
+        <View
+          style={{
+            paddingVertical: 20,
+            borderTopWidth: 1,
+            borderTopColor: '#CED0CE'
+          }}>
+          <ActivityIndicator animating size="large" />
+        </View>
+      );
+    }
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              roundAvatar
+              title={`${item.name.first} ${item.name.last}`}
+              subtitle={item.email}
+              avatar={{ uri: item.picture.thumbnail }}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          keyExtractor={item => item.email}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeadeer}
+          ListFooterComponent={this.renderFooter}
+        />
+        <Button style={styles.button} onPress={() => navigate('Report')} title="Suivant"/>
+      </List>
+    );
+  }
+}
+
+class QuestionsView extends Component {
+    static navigationOptions = {
+    title: 'Questionnaire',
+  };
+
+  render() {
+    const sautDeLigne = '\n';
+    const { navigate } = this.props.navigation;
+    const textColor = this.props.selected ? "red" : "black";
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[{key: 'a'}, {key: 'b'}]}
+          renderItem={({item}) => <Text>{item.key}</Text>}
+        /><Text>{sautDeLigne}</Text>
+        <Button style={styles.button} onPress={() => navigate('Report')} title="Rapport"/>
+      </View>
+    );
+  }
+}
+
+class ReportView extends Component {
+  static navigationOptions = {
+    title: 'Rapport',
+  };
+
+  quitApp = () => {
+    Alert.alert(
+          'Exit app',
+          'Souhaitez-vous quitter ?',
+          [
+            {text: 'Non', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+            {text: 'Oui', onPress: () => BackHandler.exitApp() + console.log('je quitte')},
+          ],
+          { cancelable: false });
+
+          return true;
+    }
+
+  render() {
+    const sautDeLigne = '\n';
+    const { navigate } = this.props.navigation;
+
+    return (
+      <View style={styles.container}>
+          <Button style={styles.button} title="Quitter" onPress={this.quitApp} />
+      </View>
+    );
+  }
+}
+
+const App = createStackNavigator({
+  Test: {screen: MyWeb},
+  Welcome: {screen: WelcomeView},
+  Questions: {screen: QuestionsView},
+  Report: {screen: ReportView},
+  FlatLDemo: {screen: FlatListDemo}
+});
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+  text: {
+    fontSize:18,
+    color: 'black',
+  },
+  button: {
+    color: 'black',
+    backgroundColor: 'white'
+  },
+});
---
+++ utiles.js
@@ -0,0 +1,180 @@
+//FlatList simple
+
+<View style={styles.container}>
+  <FlatList
+    data={[{key: 'a'}, {key: 'b'}]}
+    renderItem={({item}) => <Text>{item.key}</Text>}
+  /><Text>{sautDeLigne}</Text>
+  <Button style={styles.button} onPress={() => navigate('Report')} title="Rapport"/>
+</View>
+
+// Muliple selection => ne marche pas comme cela, doit manquer des données pour afficher quelque chose
+
+class MyListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.id);
+  };
+  render() {
+    const textColor = this.props.selected ? "red" : "black";
+    return (
+      <TouchableOpacity onPress={this._onPress}>
+        <View>
+          <Text style={{ color: textColor }}>
+            {this.props.title}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+class MultiSelectList extends React.PureComponent {
+  state = {selected: (new Map(): Map<string, boolean>)};
+
+  _keyExtractor = (item, index) => item.id;
+
+  _onPressItem = (id: string) => {
+    // updater functions are preferred for transactional updates
+    this.setState((state) => {
+      // copy the map rather than modifying state.
+      const selected = new Map(state.selected);
+      selected.set(id, !selected.get(id)); // toggle
+      return {selected};
+    });
+  };
+
+  _renderItem = ({item}) => (
+    <MyListItem
+      id={item.id}
+      onPressItem={this._onPressItem}
+      selected={!!this.state.selected.get(item.id)}
+      title={item.title}
+    />
+  );
+
+  render() {
+    return (
+      <FlatList
+        data={this.props.data}
+        extraData={this.state}
+        keyExtractor={this._keyExtractor}
+        renderItem={this._renderItem}
+      />
+    );
+  }
+}
+
+// Demo avec données d'API
+class FlatListDemo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false,
+    };
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState({ loading: true });
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Coming soon...</Text>
+      </View>
+    );
+  }
+}
+
+// Modal
+class ModalExample extends Component {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+//WbView rendre une page internet dans le bon format pour natel
+class MyWeb extends Component {
+  render() {
+    return (
+      <WebView
+        source={{uri: 'https://github.com/facebook/react-native'}}
+        style={{marginTop: 20}}
+      />
+    );
+  }
+}
+
+// // Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyCzyUAWdHzoYcPSvZUuuihQlqj1okchhgQ",
+//   authDomain: "projet-tb-121585.firebaseapp.com",
+//   databaseURL: "https://projet-tb-121585.firebaseio.com",
+//   projectId: "projet-tb-121585",
+//   storageBucket: "projet-tb-121585.appspot.com",
+//   messagingSenderId: "753741310354"
+// };
+// firebase.initializeApp(config);
